15aa3cbb7aa418fedd528575f7aa08e45da70b95
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-04 14:54:33 -0700

    feat: add Google Tag Manager GTM-T9SSRVJ4

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import './globals.css';
+import Script from 'next/script';
 import { Inter, Instrument_Serif, Cormorant_Garamond } from 'next/font/google';
 import { SITE_DESCRIPTION, SITE_NAME, SITE_URL } from '@/lib/site';
+
+const GTM_ID = 'GTM-T9SSRVJ4';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter', display: 'swap' });
 const instrument = Instrument_Serif({ subsets: ['latin'], variable: '--font-instrument', weight: '400', display: 'swap' });
@@ -27,7 +30,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.variable + ' ' + instrument.variable + ' ' + cormorant.variable}>
-      <body>{children}</body>
+      <Script id="google-tag-manager" strategy="beforeInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+      </Script>
+      <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
